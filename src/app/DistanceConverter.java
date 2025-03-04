cd67e308835c00713905a07363b98e411aea3ca7
melmanss <74724829+melmanss@users.noreply.github.com>
2025-03-04 15:18:27 +0200

    Update DistanceConverter.java

--- src/app/DistanceConverter.java
+++ src/app/DistanceConverter.java
@@ -1,21 +1,17 @@
 package app;
-
 import java.util.Scanner;
 
 public class DistanceConverter {
     public static double milesToKilometers(double miles) {
 
         System.out.println("App for distance converting");
-
         System.out.println("Version 1.0.");
 
         return miles * 1.60934;
     }
-
     public static double kilometersToMiles(double kilometers) {
         return kilometers / 1.60934;
     }
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
